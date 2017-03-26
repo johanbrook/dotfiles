5cc90ef7fea20e43be8a1efc8a08620f439ca35b
Johan Brook <johan.jvb@gmail.com>
2017-03-26 18:37:38 +0200

    Fish stuff

---
+++ dependencies.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+brew update
+brew install fish diff-so-fancy
---
+++ fish/bootstrap.sh
@@ -0,0 +1,3 @@
+# Run in a Fish shell
+curl -L http://get.oh-my.fish | fish
+set -U fish_user_paths $HOME/.dotfiles/bin $fish_user_paths
