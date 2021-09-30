fc31d1e896e9f74de3ceb605d19525cd036e06fa
Johan Brook <johan@johanbrook.com>
2021-09-30 21:41:00 +0200

    Update fish conf

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,9 +1,8 @@
-# nvm dir
-set -gx NVM_DIR ~/.nvm
+# remove greeting message on new shell
+set fish_greeting
 
-# TTC dashboard (https://github.com/notwaldorf/tiny-care-terminal/blob/master/sample.env)
-set -gx TTC_WEATHER 'Gothenburg'
-set -gx TTC_REPOS '~/Projects/Code/lookback/lookback,~/Projects/Code/lookback/lookback-upload-api,~/Projects/Code/lookback/lookback-signal-server,~/Projects/Code/lookback/lookback-participate-web,~/Projects/Code/lookback/lookback-website'
+setenv EDITOR micro
 
-set -gx TTC_APIKEYS false
+setenv LANG "en_GB.UTF-8"
 
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
