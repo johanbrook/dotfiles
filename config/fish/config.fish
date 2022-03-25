d9a4d583a685d9c0e52f4d5b1b5eb7a689454aef
Johan Brook <johan@johanbrook.com>
2022-03-25 09:32:13 +0100

    Fix fish user paths

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -5,4 +5,6 @@ setenv EDITOR micro
 
 setenv LANG "en_GB.UTF-8"
 
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+setenv DENO_INSTALL "~/.deno"
+
+fish_add_path ~/.dotfiles/bin /usr/local/sbin ~/.deno/bin ~/Library/Application\ Support/edgedb/bin
